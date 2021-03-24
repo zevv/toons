dcebc787bfad79e516e40ff7487465c43dd19b2e
Ico Doornekamp <ico@zevv.nl>
2021-03-24 19:46:26 +0100

    captures

--- lib/xpeg.ex
+++ lib/xpeg.ex
@@ -1,105 +1,111 @@
 defmodule Xpeg do
   
   require Logger
-  
-  defp mk_choice(p1, p2) do
-    List.flatten [
-      {:choice, length(p1)+2, length(p1)+length(p2)+2},
-      p1,
-      {:commit},
-      p2 # <- backtrack
-    ] # <- commit
+
+  # Emit a choice/commit pair around pattern p; off_back and off_commit are the
+  # offsets to the backtrack and commit targets, relative to the commit
+  # instruction
+  defp choice_commit(p, off_commit, off_back) do
+    [{:choice, off_back, off_commit}] ++ p ++ [{:commit}]
   end
-  
+
+  # Generic ordered choice
+  defp mk_choice(p1, p2) do
+    choice_commit(p1, length(p1)+length(p2)+2, length(p1)+2) ++ p2
+  end
+
+  # kleene-star-operator for sets make a :span
   defp mk_star([set: cs]) do
     [{:span, cs}]
   end
 
+  # Generic kleene-star operator
   defp mk_star(p) do
-    List.flatten [
-      {:choice, length(p)+2, 0}, # <- commit
-      p,
-      {:commit}
-      ] # <- backtrack
+    choice_commit(p, 0, length(p)+2)
   end
 
+  # Generic ! 'not' predicate
   defp mk_not(p) do
-    List.flatten [
-      {:choice, length(p)+3, length(p)+2},
-      p,
-      {:commit},
-      {:fail} # <- commit
-    ] # <- backtrack
+    choice_commit(p, length(p)+2, length(p)+3) ++ [{:fail}]
   end
 
-  defp mk_question(p) do
-    List.flatten [
-      {:choice, length(p)+2, length(p)+2},
-      p,
-      {:commit}, 
-    ] # <- backtrack | commit
+  # Generic optional
+  defp mk_optional(p) do
+    choice_commit(p, length(p)+2, length(p)+2)
   end
-  
+
+  # Minus for sets is the difference between sets
   defp mk_minus([set: cs1], [set: cs2]) do
     [set: MapSet.difference(cs1, cs2)]
   end
 
+  # Generic minus, !p2 * p1
   defp mk_minus(p1, p2) do
     List.flatten [ mk_not(p2), p1 ]
   end
-
+  
   # Transform AST tuples into PEG IR
-
   defp parse({ id, _meta, args }) do
     #IO.inspect {"parse", id, args }
     case { id, args } do
-      # List of rules
+
+      # List of named rules
       { :__block__, ps } ->
         Enum.map(ps, &parse/1) |> List.flatten
-      # Named rules
-      { :<-, [{label, _, nil}, patt] } ->
+
+      # Named rule
+      { :<-, [{label, _, nil}, patt] }            ->
         [{label, parse(patt) ++ [{:return}]}]
       { :<-, [{:__aliases__, _, [label]}, patt] } ->
         [{label, parse(patt) ++ [{:return}]}]
+
       # '*' Concatenation
       { :*, [p1, p2] } ->
         parse(p1) ++ parse(p2)
+
       # '|' Ordered choice
       { :|, [p1, p2] } ->
         mk_choice parse(p1), parse(p2)
+
       # '*' zero-or-more operator
       { :star, [p] } ->
         mk_star parse(p)
+
       # '?' one-or-zero operator
       { :question, [p] } ->
-        mk_question(parse(p))
+        mk_optional(parse(p))
+
       # '+' one-or-more operator
       { :+, [p] } ->
-        p = parse(p)
-        p ++ mk_star(p)
-      { :-, [p1, p2] } ->
+        p = parse(p); p ++ mk_star(p)
+        { :-, [p1, p2] } ->
         mk_minus parse(p1), parse(p2)
+
       # '!' 'not' operator
       { :!, [p] } ->
         mk_not parse(p)
+
       # Charset
       { :{}, p } ->
-        IO.inspect {"set", p}
         [{ :set, parse_set(p) }]
+
       # Repetition count
       {{:., _, [Access, :get]}, [p, count]} ->
-        IO.inspect {"repeat"}
         List.duplicate parse(p), count
+
       # Call
-      { label, nil } ->
+      { label, nil }          ->
         [{:call, label}]
-      {:__aliases__, [label]} ->
+      { :__aliases__, [label]} ->
         [{:call, label}]
+
+      # Capture
+      { :cap, [p] } ->
+        List.flatten [{:capopen}, parse(p), {:capclose}]
     end
   end
 
   # Transform AST literals into PEG IR
-
   defp parse(p) do
     case p do
       0 -> [{ :nop }]
@@ -107,12 +113,11 @@ defmodule Xpeg do
       v when is_number(v) -> [{ :any, v }]
       v when is_binary(v) -> to_charlist(v) |> Enum.map(fn c -> { :chr, c } end)
       [v] -> [{ :chr, v }]
-      v -> IO.inspect {"Unhandled lit", v}
+      v -> raise("Unhandled lit: #{inspect(v)}")
     end
   end
 
   # Transform AST character set to PEG IR. `{'x','y','A'-'F','0'}`
-
   defp parse_set(ps) do
     Enum.map(ps, fn p ->
       case p do
@@ -124,47 +129,51 @@ defmodule Xpeg do
     |> List.flatten
     |> MapSet.new
   end
+  
+  # PEG compilation macro: takes a grammar description in Elixir-AST
+  defmacro peg(start, [{:do, v}]) do
+    %{
+      start: start,
+      rules: parse(v),
+    }
+    |> IO.inspect
+    |> Macro.escape
+  end
+
+
 
   # Error handling: backtrack if possible, error out otherwise
-
   defp backtrack(state) do
-    Logger.debug("backtrack")
+    Logger.debug("<<<")
     case state.back_stack do
       [frame | back_stack] ->
         state = %{state | back_stack: back_stack, ret_stack: frame.ret_stack }
-        #IO.inspect {"error", frame.s, state}
         match(frame.patt_back, frame.s, state)
       [] ->
         %{state | result: :error}
     end
   end
 
-  # Match a subjects against a grammar
-
-  defp match(grammar, s) do
-    state = %{
-      grammar: grammar,
-      back_stack: [],
-      ret_stack: [],
-      result: :unknown
-    }
-    state = case grammar.rules[grammar.start] do
-      nil -> raise "could not find initial rule '#{grammar.start}'"
-      patt -> match(patt, to_charlist(s), state)
-    end
-    state.result
-  end
-
   # Execute PEG IR to match the passed subject charlist
+  defp match([inst|ptail]=patt, s, state) do
 
-  defp match(patt, s, state) do
-
-    [inst | ptail] = patt
-    Logger.debug("#{inspect(inst)} - '#{inspect(s) |> String.slice(1..10)}'")
+    ds = s |> inspect |> String.slice(1, 15) |> String.pad_trailing(15)
+    di = inst |> Tuple.to_list |> Enum.map(&inspect/1) |> Enum.join(" ")
+    Logger.debug("#{ds}|#{di}")
 
     case inst do
 
-      { :chr, c } -> 
+      { :nop } ->
+        match(ptail, s, state)
+
+      { :any, count } ->
+        if length(s) >= count do
+          match(ptail, Enum.drop(s, count), state)
+        else
+          backtrack(state)
+        end
+
+      { :chr, c } ->
         if length(s) > 0 and c == hd(s) do
           match(ptail, tl(s), state)
         else
@@ -177,7 +186,7 @@ defmodule Xpeg do
         else
           backtrack(state)
         end
-      
+
       { :span, cs } ->
         s = Enum.drop_while(s, fn c -> MapSet.member?(cs, c) end)
         match(ptail, s, state)
@@ -189,21 +198,13 @@ defmodule Xpeg do
           ret_stack: state.ret_stack,
           s: s,
         }
-        state = %{state | :back_stack => [ frame | state.back_stack ]}
+        state = %{state | :back_stack => [frame | state.back_stack]}
         match(ptail, s, state)
 
       { :commit } ->
         [frame | back_stack] = state.back_stack
         state = %{state | :back_stack => back_stack}
-        #Logger.debug "commit #{inspect(frame)}" 
         match(frame.patt_commit, s, state)
-
-      { :any, count } ->
-        if length(s) >= count do
-          match(ptail, Enum.drop(s, count), state)
-        else
-          backtrack(state)
-        end
 
       { :call, label } ->
         state = %{state | :ret_stack => [ptail | state.ret_stack]}
@@ -212,18 +213,21 @@ defmodule Xpeg do
           patt -> match(patt, s, state)
         end
 
+      { :capopen } ->
+        state = %{state | :cap_stack => [ {:open, length(s), s} | state.cap_stack]}
+        match(ptail, s, state)
+
+      { :capclose } ->
+        state = %{state | :cap_stack => [ {:close, length(s), s} | state.cap_stack]}
+        match(ptail, s, state)
+
       { :return } ->
         case state.ret_stack do
           [patt | ret_stack] ->
-            #Logger.debug "return #{inspect(patt)}"
             match(patt, s, %{state | :ret_stack => ret_stack})
           [] ->
-            Logger.debug("done, leftover: #{s}")
             %{state | :result => :ok}
         end
-
-      { :nop } ->
-        match(ptail, s, state)
 
       { :fail } ->
         backtrack(state)
@@ -231,63 +235,85 @@ defmodule Xpeg do
     end
   end
 
-  # PEG compilation macro: takes a grammar description in Elixir-AST
- 
-  defmacro peg(start, grammar) do
-    [ {:do, v } ] = grammar
-    %{
-      start: start,
-      rules: parse(v),
+
+  # Match a subject against a grammar
+  defp match(grammar, s) do
+    state = %{
+      grammar: grammar,
+      back_stack: [],
+      ret_stack: [],
+      cap_stack: [],
+      result: :unknown
     }
-    |> IO.inspect
-    |> Macro.escape
+    case grammar.rules[grammar.start] do
+      nil -> raise "could not find initial rule '#{grammar.start}'"
+      patt -> match(patt, to_charlist(s), state)
+    end
+    |> collect_captures
   end
 
-  # Single-pattern compilation macro
-  defmacro patt(p) do
-    Macro.escape parse(p).result
+  # Flatten the cap stack and collect the captures
+  def collect_captures(state) do
+    state.cap_stack
+    |> Enum.reverse
+    |> Enum.reduce({[], []}, fn frame, {acc, caps} ->
+      case { frame, acc } do
+        {{:open, _, _ }, _} ->
+          {[frame | acc], caps}
+        {{:close, oc, _sc }, [{:open, oo, so} | t]} ->
+          {t, [Enum.take(so, oo-oc) | caps]}
+      end
+    end)
   end
-  
 
   def go do
 
-    if true do
-      p = peg :JSON do
-
-        S              <- star({' ','\t','\r','\n'})
-        True           <- "true"
-        False          <- "false"
-        Null           <- "null"
-
-        Xdigit         <- {'0'-'9','a'-'f','A'-'F'}
-        UnicodeEscape  <- 'u' * Xdigit[4]
-       
-        Escape         <- '\\' * ({ '"', '\\', '/', 'b', 'f', 'n', 'r', 't' } | UnicodeEscape)
-        StringBody     <- star(Escape) * star( +( {32-255} - {'"'} - {'\\'}) * star(Escape) )
-        String         <- '"' * StringBody * '"'
-
-        Minus          <- '-'
-        IntPart        <- '0' | {'1'-'9'} * star{'0'-'9'}
-        FractPart      <- "." * +{'0'-'9'}
-        ExpPart        <- ( 'e' | 'E' ) * question( '+' | '-' ) * +{'0'-'9'}
-        Number         <- question(Minus) * IntPart * question(FractPart) * question(ExpPart)
-
-        DOC            <- Value * !1
-        ObjPair        <- S * String * S * ":" * Value
-        Object         <- '{' * ( ObjPair * star( "," * ObjPair ) | S ) * "}"
-        Array          <- "[" * ( Value * star( "," * Value ) | S ) * "]"
-        Value          <- S * ( Number | String | Object | Array | True | False | Null ) * S
-
-        JSON           <- Value * !1
-
-      end
-
-      match(p, """
-        [ "look", "at", "this", { "thing": "parseing", "json": 3.1415 }, true, false ]
-      """)
+    p = peg :flop do
+      word <- cap(star({'a'-'z'}))
+      flop <- cap(word * star(',' * word))
     end
 
+    match(p, "one,two,three")
+
   end
+
+  #def gojs do
+
+  #  p = peg :JSON do
+
+  #    S              <- star({' ','\t','\r','\n'})
+  #    True           <- "true"
+  #    False          <- "false"
+  #    Null           <- "null"
+
+  #    Xdigit         <- {'0'-'9','a'-'f','A'-'F'}
+  #    UnicodeEscape  <- 'u' * Xdigit[4]
+
+  #    Escape         <- '\\' * ({ '"', '\\', '/', 'b', 'f', 'n', 'r', 't' } | UnicodeEscape)
+  #    StringBody     <- star(Escape) * star( +( {32-255} - {'"'} - {'\\'}) * star(Escape) )
+  #    String         <- '"' * StringBody * '"'
+
+  #    Minus          <- '-'
+  #    IntPart        <- '0' | {'1'-'9'} * star{'0'-'9'}
+  #    FractPart      <- "." * +{'0'-'9'}
+  #    ExpPart        <- ( 'e' | 'E' ) * question( '+' | '-' ) * +{'0'-'9'}
+  #    Number         <- question(Minus) * IntPart * question(FractPart) * question(ExpPart)
+
+  #    DOC            <- Value * !1
+  #    ObjPair        <- S * String * S * ":" * Value
+  #    Object         <- '{' * ( ObjPair * star( "," * ObjPair ) | S ) * "}"
+  #    Array          <- "[" * ( Value * star( "," * Value ) | S ) * "]"
+  #    Value          <- S * ( Number | String | Object | Array | True | False | Null ) * S
+
+  #    JSON           <- Value * !1
+
+  #  end
+
+  #  match(p, """
+  #    [ "look", "at", "this", { "thing": "parseing", "json": 3.1415 }, true, false ]
+  #  """)
+
+  #end
 
 end
 
